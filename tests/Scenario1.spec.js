483e7726d7457b5ac244d70514c8d86c64403d79
Atchuthan Soorasangaram <atchuthan98@googlemail.com>
2025-02-09 22:19:38 +0000

    Scenario 1 Pass

--- tests/Scenario1.spec.js
+++ tests/Scenario1.spec.js
@@ -2,9 +2,16 @@
 const { test, expect }= require('@playwright/test');
 
 // Write Test
-test('Make a GET request to the endpoint', async(request)=>{
+test('Make a GET request to the endpoint', async({request})=>{
 
-  const getApiResponse = await request.get('six-nations')
-
+  const startTime = Date.now();
+  const getApiResponse = await request.get('/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:six-nations')
+  const endTime = Date.now();
+  const responseTime = endTime-startTime
   console.log(await getApiResponse.json());
+  console.log(responseTime);
+  expect(getApiResponse.ok()).toBeTruthy();
+  expect(getApiResponse.status()).toBe(200);
+  expect(responseTime).toBeLessThan(1000);
+  
 })
