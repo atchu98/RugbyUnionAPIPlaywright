c826840c300d51fe01f6961ee626c2416757968a
Atchuthan Soorasangaram <atchuthan98@googlemail.com>
2025-02-10 08:29:02 +0000

    Create Scenario5.spec.js

---
+++ tests/Scenario5.spec.js
@@ -0,0 +1,26 @@
+// Load Playwright
+const { test, expect }= require('@playwright/test');
+
+// Write Test
+test('Make a GET request to the endpoint including the header x-test-harness set to true', async({request})=>{
+
+  const endpoint = '/wc-poll-data/container/sport-standings?urn=urn:bbc:sportsdata:rugby-union:tournament:six-nations';
+
+  // Send GET request with required headers
+  const getApiResponse = await request.get(endpoint, {
+    headers: { 'x-test-harness': 'true' },
+  });
+
+  // Parse response headers
+  const responseHeaders = await getApiResponse.headers();
+
+  const responseBody = await getApiResponse.json()
+  console.log(responseBody);
+  console.log('Response Status:', getApiResponse.status());
+  console.log('Response Headers:', responseHeaders);
+
+  // Assertions
+  expect(getApiResponse.ok()).toBeTruthy(); 
+  //expect(responseHeaders).toHaveProperty('x-test-harness');
+  //expect(responseHeaders['x-test-harness']).toBe('true'); 
+})
